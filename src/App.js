5f210e268292cfb701eaa09ec656256559c3ce7b
Neill Bogie <neillbogie@googlemail.com>
2020-05-08 18:49:37 +0100

    initial soln to level 100 in react
    Intentionally no react idioms,
    and all components in a single file.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
+//Known issues: 
+// Wrongly assumes all episodes have images (correct for G.o.T. data)
+// Intentionally keeps all components in a single file
+//
+// TODO Can we delete service worker?
+//
+// TODO Best way to fix/remove tests?
+
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import EpisodesData from './EpisodesData.json';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <EpisodeList episodes={EpisodesData} />
+      <Footer />
     </div>
   );
 }
+
+
+function Header() {
+  return (<header>Level 100 Sample Solution - React</header>);
+}
+
+
+function EpisodeList(props) {
+  return (
+    <div className="episode-list">
+      {props.episodes.map((episode) => <Episode key={episode.id} episode={episode} />)}
+    </div>
+  )
+}
+
+function Episode(props) {
+  return (
+    <div className="card">
+      <h1>{props.episode.name} - {makeEpisodeCode(props.episode)}</h1>
+      <img src={props.episode.image.medium} alt="screengrab of episode" />
+      <p>{props.episode.summary}</p>
+    </div>
+  );
+}
+
+
+function Footer() {
+  return (
+    <footer>
+      Credits: Episode data is originally
+      from the fantastic <a href="https://tvmaze.com/">TVmaze</a> via
+      their <a href="https://www.tvmaze.com/api">API</a>.
+    </footer>
+  )
+}
+
+
+//==== NOT components - some utility functions ================================
+
+//return an episode code to represent the given episode, e.g. "S01E03"
+function makeEpisodeCode(episode) {
+  return `S${pad(episode.season)}E${pad(episode.number)}`;
+}
+
+//pad a number out to be an (at least) 2-digit string
+//e.g. pad(3) -> "03"
+function pad(num) {
+  return num.toString().padStart(2, "0");
+}
+
 
 export default App;
