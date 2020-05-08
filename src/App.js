4b92491c333a6251d6d33445a7724fe5d12533f9
Neill Bogie <neillbogie@googlemail.com>
2020-05-09 00:59:17 +0100

    complete level-350 (no extras)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import React, { useState, useEffect } from "react";
 import Header from './Header.js';
 import Footer from './Footer.js';
 import SearchableEpisodeList from './SearchableEpisodeList';
-import StaticEpisodesData from './EpisodesData.json';
 
 import './App.css';
 
 
 function App() {
-
-  //you can turn this off during development to save data
-  const useLiveData = true;
 
   //STATE HOOK: the fetched episodes - initially none
   const [episodes, setEpisodes] = useState([]);
@@ -21,11 +17,7 @@ function App() {
 
   //EFFECT HOOK: Fetch episodes from API on component mount, only.
   useEffect(() => {
-    if (useLiveData) {
-      fetchEpisodesForShow(179);
-    } else {
-      pretendToFetchEpisodes();
-    }
+    fetchEpisodesForShow(179)
   }, []); //Note: IMPORTANT don't forget [] as last param so 
   //      that useEffect does not execute on any state change
   //      (INCLUDING the one it instigates)
@@ -34,21 +26,10 @@ function App() {
 
   async function fetchEpisodesForShow(showId) {
     setLoading(true);
-    console.log("Fetching data for real...");
     const result = await fetch(`https://api.tvmaze.com/shows/${showId}/episodes`);
     //Note: we should handle errors here
     const json = await result.json();
     setEpisodes(json);
-    setLoading(false);
-  };
-
-
-  async function pretendToFetchEpisodes() {
-    console.log("Using static fake data");
-    setLoading(true);
-    //Artificially delay to let devs see the loading screen
-    await delay(2000);
-    setEpisodes(StaticEpisodesData);
     setLoading(false);
   };
 
@@ -70,11 +51,6 @@ function App() {
 
     </div>
   );
-}
-
-async function delay(timeInMillis) {
-  //ignore this
-  return (new Promise((a, b) => setTimeout(a, timeInMillis)));
 }
 
 
