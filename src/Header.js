3dc5e344b4ef2381e5879d91c036e3d5f457ba47
Neill Bogie <neillbogie@googlemail.com>
2020-05-08 20:27:49 +0100

    retitled resources to level-200

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Header() {
-    return (<header>Level 101 Sample Solution - React</header>);
+    return (<header>Level 200 Sample Solution - React</header>);
 }
 
 export default Header;
