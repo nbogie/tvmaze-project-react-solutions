3a16bf3daf8944b6487532d8fbfb07782ab3fa46
Neill Bogie <neillbogie@googlemail.com>
2020-05-09 03:39:18 +0100

    renumber to level 500

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Header() {
-    return (<header>Level 400 Sample Solution - React</header>);
+    return (<header>Level 500 Sample Solution - React</header>);
 }
 
 export default Header;
